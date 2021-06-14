89c29515c1fb9628f012652fb0d7862ee602505d
Brianna Bias <brianna.bias@gmail.com>
2021-06-14 01:01:12 -0700

    we need index?

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import description from './Assets/description.png';
 
 export default class Body extends Component {
     render() {
         return (
             <div>
-                
+                <img src={description} alt='description' />
             </div>
         )
     }
