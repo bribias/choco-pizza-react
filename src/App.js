eb808d9a5dc9f7296eabda93e01e5180c49bc770
Brianna Bias <brianna.bias@gmail.com>
2021-06-14 00:49:45 -0700

    more wireframe fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Body />
-        <Footer />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
